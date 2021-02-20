165dfdd996d8a0602fccf7151c38fdae99411a65
successs404 <neoruien@u.nus.edu>
2021-02-20 12:17:09 +0800

    Updates

---
+++ precache-manifest.2a4b153171912e9efebd967cc4867866.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7929debe56bda7c42d6",
+    "url": "/profile-page/static/css/main.9d2889f9.chunk.css"
+  },
+  {
+    "revision": "e7929debe56bda7c42d6",
+    "url": "/profile-page/static/js/main.e7929deb.chunk.js"
+  },
+  {
+    "revision": "b5241827a03b6c197168",
+    "url": "/profile-page/static/js/1.b5241827.chunk.js"
+  },
+  {
+    "revision": "65aa41316dcf41e6ae92",
+    "url": "/profile-page/static/js/2.65aa4131.chunk.js"
+  },
+  {
+    "revision": "e2ac9e689bef174b9438",
+    "url": "/profile-page/static/js/runtime~main.e2ac9e68.js"
+  },
+  {
+    "revision": "acd2e6ff6512b8a2ea16bfb98bbacdda",
+    "url": "/profile-page/static/media/image.acd2e6ff.jpg"
+  },
+  {
+    "revision": "765e5404a107af336b71162914eefa78",
+    "url": "/profile-page/static/media/phone.765e5404.svg"
+  },
+  {
+    "revision": "9a7c723f174e840a1a44e8645f18b6df",
+    "url": "/profile-page/static/media/shop.9a7c723f.svg"
+  },
+  {
+    "revision": "3ecb5e93c5799ec7fa2ef5b16240f2c6",
+    "url": "/profile-page/index.html"
+  }
+];
